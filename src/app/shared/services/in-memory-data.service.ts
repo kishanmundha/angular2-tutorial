3839864365745a7c39ab45347575c40d9edf6aac
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-24 21:47:26 +0530

    commit

--- src/app/shared/services/in-memory-data.service.ts
+++ src/app/shared/services/in-memory-data.service.ts
@@ -1,22 +1,9 @@
 export class InMemoryDataService {
     createDb() {
-        const heroes = [
-            { id: 11, name: 'Mr. Nice' },
-            { id: 12, name: 'Narco' },
-            { id: 13, name: 'Bombasto' },
-            { id: 14, name: 'Celeritas' },
-            { id: 15, name: 'Magneta' },
-            { id: 16, name: 'RubberMan' },
-            { id: 17, name: 'Dynama' },
-            { id: 18, name: 'Dr IQ' },
-            { id: 19, name: 'Magma' },
-            { id: 20, name: 'Tornado' }
-        ];
-
         const products = [
             {
                 id: 1,
-                name: 'Sendwich',
+                name: 'Sandwich',
                 description: 'Descript line',
                 image: 'https://theambrosia.com/476/veg-sandwich.jpg',
                 price: 20,
@@ -48,6 +35,6 @@ export class InMemoryDataService {
             }
         ];
 
-        return { heroes, products };
+        return { products };
     }
 }
