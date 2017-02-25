41b70654db46d372763b1e432dfe9dbe22777eb5
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-25 14:45:26 +0530

    Dynamic Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  products = [{
+    name: 'Burger',
+    price: 33
+  }, {
+    name: 'French Fries',
+    price: 66
+  }, {
+    name: 'Pizza',
+    price: 90
+  }, {
+    name: 'Dahi Vada',
+    price: 40
+  }, {
+    name: 'Sandwich',
+    price: 20
+  }];
 }
