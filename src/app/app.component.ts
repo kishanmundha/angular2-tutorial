ed8881f91bebd25f62a18db8158eba268c9d91c6
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-25 14:53:09 +0530

    Product interface add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Product } from './shared/interfaces';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  products = [{
+  products: Product[] = [{
     name: 'Burger',
     price: 33
   }, {
