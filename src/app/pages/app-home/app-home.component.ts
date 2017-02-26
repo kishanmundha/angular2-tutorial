472816ef65c411fe0086c55c22ed1db2b19da892
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-26 12:42:32 +0530

    XHR & Dependency Injection

--- src/app/pages/app-home/app-home.component.ts
+++ src/app/pages/app-home/app-home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../../shared/interfaces';
 
+import { AppProductService } from '../../shared/services/app-product.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './app-home.component.html',
@@ -9,29 +11,15 @@ import { Product } from '../../shared/interfaces';
 })
 export class AppHomeComponent implements OnInit {
 
-  products: Product[] = [{
-    name: 'Burger',
-    price: 33
-  }, {
-    name: 'French Fries',
-    price: 66
-  }, {
-    name: 'Pizza',
-    price: 90
-  }, {
-    name: 'Dahi Vada',
-    price: 40
-  }, {
-    name: 'Sandwich',
-    price: 20
-  }];
+  products: Product[] = [];
 
   sort = 'name';
   search = '';
 
-  constructor() { }
+  constructor(private appProductService: AppProductService) { }
 
   ngOnInit() {
+    this.appProductService.getProducts().then(products => { this.products = products; });
   }
 
 }
