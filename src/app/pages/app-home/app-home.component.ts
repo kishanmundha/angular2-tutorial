184ca228ed8480f78b2d3327d59efce48e509a3d
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-27 00:51:57 +0530

    Animations

--- src/app/pages/app-home/app-home.component.ts
+++ src/app/pages/app-home/app-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, animate, transition, style } from '@angular/core';
 
 import { Product } from '../../shared/interfaces';
 
@@ -7,7 +7,16 @@ import { AppProductService } from '../../shared/services/app-product.service';
 @Component({
   selector: 'app-home',
   templateUrl: './app-home.component.html',
-  styleUrls: ['./app-home.component.scss']
+  styleUrls: ['./app-home.component.scss'],
+  animations: [
+    trigger('dataLoaded', [
+      transition('void => *', [
+        style({ 'margin-top': '50px', opacity: 0 }),
+        animate(500, style({ 'margin-top': '0px', opacity: 0.5 })),
+        animate(500, style({ opacity: 1 }))
+      ]),
+    ])
+  ]
 })
 export class AppHomeComponent implements OnInit {
 
@@ -16,10 +25,12 @@ export class AppHomeComponent implements OnInit {
   sort = 'name';
   search = '';
 
+  dataLoaded = false;
+
   constructor(private appProductService: AppProductService) { }
 
   ngOnInit() {
-    this.appProductService.getProducts().then(products => { this.products = products; });
+    this.appProductService.getProducts().then(products => { this.products = products; this.dataLoaded = true; });
   }
 
 }
