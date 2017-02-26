c5dd9d0b4a147d4b873adae755e92e328a8176e9
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-27 00:03:49 +0530

    Lazy loading module

---
+++ src/app/modules/feedback/feedback.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { FeedbackSuccessComponent } from './pages/feedback-success/feedback-success.component';
+
+export const routes: Routes = [
+  { path: '', component: FeedbackComponent },
+  { path: 'success', component: FeedbackSuccessComponent}
+];
