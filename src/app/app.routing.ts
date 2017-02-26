d3375e8db784b6d9d23f2b3c9cb25ddb495c24ef
Kishan Mundha <kishan.mundha@gmail.com>
2017-02-26 19:40:33 +0530

    Form control

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,13 @@ import { Routes } from '@angular/router';
 import { AppHomeComponent } from './pages/app-home/app-home.component';
 import { AppNotFoundComponent } from './pages/app-not-found/app-not-found.component';
 import { AppCartComponent } from './pages/app-cart/app-cart.component';
+import { AppCheckoutComponent } from './pages/app-checkout/app-checkout.component';
+import { AppCheckoutSuccessComponent } from './pages/app-checkout-success/app-checkout-success.component';
 
 export const routes: Routes = [
   { path: '', component: AppHomeComponent },
   { path: 'cart', component: AppCartComponent },
+  { path: 'checkout', component: AppCheckoutComponent },
+  { path: 'checkout/success', component: AppCheckoutSuccessComponent },
   { path: '**', component: AppNotFoundComponent }
 ];
--- src/app/shared/interfaces/index.ts
+++ src/app/shared/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './product.interface';
+export * from './user-info-checkout.interface';
